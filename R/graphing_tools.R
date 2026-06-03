6f2326349d67d2e14317aa7454aee551ee231211
James Peng <jamespeng@Jamess-MacBook-Air-974.local>
2026-06-02 20:04:23 -0700

    updating vignette'

--- R/graphing_tools.R
+++ R/graphing_tools.R
@@ -233,7 +233,7 @@ graph_individual_quantiles <- function(
           group = id
         )
       ) +
-        ggplot2::geom_line(alpha = 0.5, linewidth = 0.4) +
+        ggplot2::geom_line(alpha = 0.75, linewidth = 0.4) +
         ggplot2::labs(
           x = "Quantile level",
           y = "Quantile value",
@@ -284,7 +284,7 @@ graph_individual_quantiles <- function(
         color = palette[[id]],
         width = 1
       ),
-      opacity = 0.5,
+      opacity = 0.75,
       showlegend = TRUE
     )
   }
@@ -301,7 +301,7 @@ graph_individual_quantiles <- function(
     p,
     "
     function(el, x) {
-      var fadedOpacity = 0.5;
+      var fadedOpacity = 0.75;
       var fadedWidth = 1;
       var activeOpacity = 1.0;
       var activeWidth = 3;
